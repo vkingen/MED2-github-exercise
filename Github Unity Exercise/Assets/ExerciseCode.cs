0c750741db8a849b736b296ec69b0fac25ed8ca6
vkingen <lassevikingkristensen@gmail.com>
2022-02-18 09:59:22 +0100

    johnjohn

--- Github Unity Exercise/Assets/ExerciseCode.cs
+++ Github Unity Exercise/Assets/ExerciseCode.cs
@@ -9,10 +9,10 @@ public class ExerciseCode : MonoBehaviour
 
     private void Start() {
         Debug.Log("value before calling function: " + myVariable);
-        
-        // Call the function 'PassByValue' below this line – pass 'myVariable' as argument.
 
-        
+        // Call the function 'PassByValue' below this line – pass 'myVariable' as argument.
+        PassByValue(myVariable);
+
         Debug.Log("value after calling function: " + myVariable);
     }
 
